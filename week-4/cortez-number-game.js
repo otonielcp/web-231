e31fd399c9d1877e87fe545232d6ba8bd046d3bd
Otoniel Cortez <otonielcortes1987@gmail.com>
2023-04-09 19:10:39 -0500

    fix some errors and added comments

--- week-4/cortez-number-game.js
+++ week-4/cortez-number-game.js
@@ -3,30 +3,41 @@
 // Date: 4/9/2023
 // Assignment: Web 231 Assignment 4
 
+// Generate a random number between 1 and 100
 function generateRndNum() {
     return Math.floor(Math.random() * 100) + 1;
   }
-
-const rnd = generateRndNum();
-const guessInput = document.getElementById("guessInput");
-const submitBtn = document.getElementById("submitBtn");
-const resultMsg = document.getElementById("resultMsg");
-
-submitBtn.onclick = function() {
-  chkGuess();
-};
-
-function chkGuess() {
-  const msgLessThan = "The number is less than";
-  const msgGreaterThan = "The number is greater than";
-  const msgCongrats = "Congratulations! You picked the correct number!";
-  const guess = parseInt(guessInput.value);
-
-  if (guess < rnd) {
-    resultMsg.textContent = msgLessThan;
-  } else if (guess > rnd) {
-    resultMsg.textContent = msgGreaterThan;
-  } else {
-    resultMsg.textContent = msgCongrats;
+  
+  // Assign the random number to a constant variable
+  const rnd = generateRndNum();
+  
+  // Get the input element, submit button element, and result message element
+  const guessInput = document.getElementById("guessInput");
+  const submitBtn = document.getElementById("submitBtn");
+  const resultMsg = document.getElementById("resultMsg");
+  
+  // Attach a click event listener to the submit button
+  submitBtn.onclick = function() {
+    chkGuess();
+  };
+  
+  // Check if the user's guess is correct
+  function chkGuess() {
+    // Define the messages to display
+    const msgLessThan = "The number is less than";
+    const msgGreaterThan = "The number is greater than";
+    const msgCongrats = "Congratulations! You picked the correct number!";
+  
+    // Get the user's guess from the input element
+    const guess = parseInt(guessInput.value);
+  
+    // Compare the user's guess to the random number and display the appropriate message
+    if (guess < rnd) {
+      resultMsg.textContent = msgLessThan;
+    } else if (guess > rnd) {
+      resultMsg.textContent = msgGreaterThan;
+    } else {
+      resultMsg.textContent = msgCongrats;
+    }
   }
-}
+  
